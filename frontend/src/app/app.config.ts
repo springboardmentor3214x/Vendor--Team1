69857a8e5277b663a2ae04fa81d9d32afd3cd9d0
Alekhya Golla <alekyagolla16@gmail.com>
2026-06-11 21:25:11 +0530

    Add the application shell with root routing

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
