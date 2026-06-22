b5f358692181e1b594578ae70e79287c6532d2ba
Chirag <chiraggit13@gmail.com>
2026-06-22 21:47:10 +0530

    Extend the auth interceptor with 401 handling

--- frontend/src/app/core/interceptors/http-context.ts
+++ frontend/src/app/core/interceptors/http-context.ts
@@ -1,3 +1,7 @@
 import { HttpContext, HttpContextToken } from '@angular/common/http';
 
 export const SILENT_AUTH_FAILURE = new HttpContextToken<boolean>(() => false);
+
+export function silentRequest(): HttpContext {
+  return new HttpContext().set(SILENT_AUTH_FAILURE, true);
+}
