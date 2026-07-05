2e3486085fdf5db75ae9047768c866849d05a132
avinashbarla <barlaavinash4633f@gmail.com>
2026-07-05 22:22:57 +0530

    Implement password hashing using bcrypt

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,14 +2,17 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.core.security import verify_password
 
+
 def login_user(db: Session, email: str, password: str):
 
+    # Find user by email
     user = db.query(User).filter(User.email == email).first()
 
-    if not user:
+    # User not found
+    if user is None:
         return None
 
-
+    # Check password
     if not verify_password(password, user.password):
         return None
 
