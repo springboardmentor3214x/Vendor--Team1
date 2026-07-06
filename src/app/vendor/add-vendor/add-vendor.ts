b1589f000ffb4564ce6854d0bb0f14146bb080da
chiragdebugs <chiraggit113@gmail.com>
2026-07-06 22:08:24 +0530

    Set up Angular project structure

---
+++ src/app/vendor/add-vendor/add-vendor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-vendor',
+  imports: [],
+  templateUrl: './add-vendor.html',
+  styleUrl: './add-vendor.css',
+})
+export class AddVendor {}
---
+++ src/app/vendor/vendor-details/vendor-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-details',
+  imports: [],
+  templateUrl: './vendor-details.html',
+  styleUrl: './vendor-details.css',
+})
+export class VendorDetails {}
