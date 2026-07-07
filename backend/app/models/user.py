af56b56b7819619f601cffa68958fad5d002513d
Avinash Barla <barlaavinash4633f@gmail.com>
2026-07-07 23:52:23 +0530

    Updated add user database model and password hashing logic

---
+++ backend/app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    mobile_number = Column(String(15), nullable=True)
+    password = Column(String(255), nullable=False)
+    role = Column(String(50), nullable=False)
+    account_status = Column(String(30), default="Pending Approval")
+    created_at = Column(DateTime, default=datetime.utcnow)
