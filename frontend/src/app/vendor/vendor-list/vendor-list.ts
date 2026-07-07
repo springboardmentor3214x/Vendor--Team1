42b419bc5ee1c477ef88ffd697db1831caa6942d
Alekhya Golla <alekyagolla16@gmail.com>
2026-07-07 23:59:22 +0530

    Updated set up Angular project structure logic

---
+++ frontend/src/app/vendor/vendor-list/vendor-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-list',
+  imports: [],
+  templateUrl: './vendor-list.html',
+  styleUrl: './vendor-list.css',
+})
+export class VendorList {}
