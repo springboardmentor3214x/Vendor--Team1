d05d62b726627ee3d7d469319ed773a2cb263366
chiragdebugs <chiraggit113@gmail.com>
2026-07-08 22:51:37 +0530

    Complete user authentication and profile management module

--- src/app/auth/profile/profile.ts
+++ src/app/auth/profile/profile.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrl: './profile.css'
 })
-export class Profile {}
+export class Profile {
+
+  isEditing = false;
+
+  user = {
+
+    fullName: 'John Doe',
+
+    email: 'john.doe@vrip.com',
+
+    mobile: '9876543210',
+
+    role: localStorage.getItem('vrip_role') || 'Vendor'
+
+  };
+
+  editProfile() {
+
+    this.isEditing = true;
+
+  }
+
+  saveProfile() {
+
+    this.isEditing = false;
+
+    alert('Profile Updated Successfully');
+
+    // Backend API will be connected here later
+
+  }
+
+}
