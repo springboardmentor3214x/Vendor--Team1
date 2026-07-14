4805d746aeafbcf29e73bce151d2ca87229d409f
Kaustubh Thallam <thallamkaustubh@gmail.com>
2026-07-14 10:15:26 +0530

    Add Performance API router, register all models in main.py

--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ from app.database.connection import engine
 from app.database.base import Base
 from app.models.vendor import Vendor
 from app.models.procurement import Procurement
+from app.models.delivery_performance import DeliveryPerformance
+from app.models.quality_evaluation import QualityEvaluation
+from app.models.communication_log import CommunicationLog
+from app.models.service_rating import ServiceRating
 from app.api.vendor import router as vendor_router
 from app.api.procurement import router as procurement_router
+from app.api.performance import router as performance_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,3 +19,4 @@ app = FastAPI(title=APP_TITLE)
 app.add_middleware(CORSMiddleware, allow_origins=CORS_ORIGINS, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.include_router(vendor_router)
 app.include_router(procurement_router)
+app.include_router(performance_router)
